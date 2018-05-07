294a198ba275986dbd592ffc2b7b9585343551d9
Stefano Silvi <sambora93@gmail.com>
2018-05-07 17:27:21 +0200

    last commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
-app.get('/ants', function(req,res) {
+app.get('/', function(req,res) {
   res.sendFile('/home/mrsambo/Documenti/infovis/Homework1/ants.html');
 });
 
